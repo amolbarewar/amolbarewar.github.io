de056ff68b750f779e7aac665c49726349659b93
Amol Barewar <amol.barewar@wingify.com>
2017-02-01 17:06:08 +0530

    #0: changes

--- background-worker.js
+++ background-worker.js
@@ -1,2 +1,2 @@
 var version = 2;
-importScripts('https://cdn.pushcrew.com/sw/4acd02fa7be469e91f249e3a5a96cf7b.js');
+importScripts('https://cdn.pushcrewstaging.com/sw/4acd02fa7be469e91f249e3a5a96cf7b.js');
