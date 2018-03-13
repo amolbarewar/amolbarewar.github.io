6fe274d642348af52292a32148ffd39aa504abe4
amolbarewar <amol.barewar@wingify.com>
2018-03-13 18:26:07 +0530

    #0: https

--- background-worker.js
+++ background-worker.js
@@ -1,2 +1,2 @@
 var version = 6;
-importScripts('https://cdn.pushcrew.com/sw/24ea185e46285febf3c5be73ea43f021.js');
+importScripts('https://cdn.pushcrew.com/sw/4e44cc75fccb3bbc4c4d9f21dfab29f0.js');
