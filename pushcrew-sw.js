9700462d1a87dcca47ef78852015b1963a647c74
amolbarewar <amol.barewar@wingify.com>
2018-04-23 18:09:45 +0530

    #0: fixing issue

--- pushcrew-sw.js
+++ pushcrew-sw.js
@@ -1,2 +1,2 @@
 var version = 6;
-importScripts('https://cdn.pushcrewstaging.com/sw/97e8db705052b51c4323e2c54efa6abd.js');
+importScripts('https://cdn.pushcrewstaging.com/sw/199625884301bef51be137a943923203.js');
