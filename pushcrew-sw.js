e1df1bf03d5e857b7dbc14f3dc7c9b6d1cdb2d57
amolbarewar <amol.barewar@wingify.com>
2018-04-26 17:52:21 +0530

    #0: sanchu

--- pushcrew-sw.js
+++ pushcrew-sw.js
@@ -1,2 +1,2 @@
 var version = 6;
-importScripts('https://cdn.pushcrewstaging.com/sw/199625884301bef51be137a943923203.js');
+importScripts('https://cdn.pushcrewstaging.com/sw/4aaeb78b0fbac7fff32b50371931c0ef.js');
