dcbf0c74454538a1ef5238e74902d6701320c2ac
amolbarewar <amol.barewar@wingify.com>
2018-05-30 19:29:56 +0530

    #0: changes

--- pushcrew-sw.js
+++ pushcrew-sw.js
@@ -1,2 +1,2 @@
 var version = 6;
-importScripts('https://cdn.pushcrewstaging.com/sw/e2aaf8556d206390dd1e929716e82df7.js');
+importScripts('https://cdn.pushcrewstaging.com/sw/371822cc3db2748d103f2f9c68796bd2.js');
