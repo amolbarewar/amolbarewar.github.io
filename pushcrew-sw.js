35735960657ac8016f9e313b149349a250f593c6
amolbarewar <amol.barewar@wingify.com>
2018-06-28 23:47:42 +0530

    #0: comit

--- pushcrew-sw.js
+++ pushcrew-sw.js
@@ -1,2 +1,2 @@
 var version = 6;
-importScripts('https://cdn.pushcrewstaging.com/sw/371822cc3db2748d103f2f9c68796bd2.js');
+importScripts('https://cdn.pushcrewstaging.com/sw/0b08ccacd84cf2b40fce9e04fef17ad0.js');
