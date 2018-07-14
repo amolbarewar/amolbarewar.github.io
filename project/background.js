4a9116b1f828db69c365266bf426093a88d811d8
amolbarewar <amol.barewar@wingify.com>
2018-07-14 10:42:05 +0530

    #0: changes

---
+++ project/background.js
@@ -0,0 +1,13 @@
+ //  var tablink = '';
+ //  chrome.runtime.onInstalled.addListener(function() {
+ //    chrome.storage.sync.set({color: '#3aa757'}, function() {
+ //      console.log("The color is green.");
+ //    });
+ //  });
+	// chrome.tabs.getSelected(null,function(tab) {
+	// 	var tablink = tab.url;
+	// });
+ //  if(tablink22 == 'stackoverflow.com') {
+ //  	alert('testing');
+ //  }
+ console.log('eee');
