1382a99fae9d255cda6ad75160b122d6704b99e2
amolbarewar <amol.barewar@wingify.com>
2019-06-18 11:47:53 +0530

    #0: remove file

--- background-worker.js
+++
@@ -1,2 +0,0 @@
-var version = 6;
-importScripts('https://cdn.pushcrewstaging.com/sw/4e44cc75fccb3bbc4c4d9f21dfab29f0.js');
--- pushcrew-sw.js
+++
@@ -1,2 +0,0 @@
-var version = 6;
-importScripts('https://cdn.pushcrewstaging.com/sw/53eb059c6198013bdeb5d5479b8b162f.js');
